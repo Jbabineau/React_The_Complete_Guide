2a67e2bf35553bf1dc61f453d697ddd203f712d6
James Babineau <jbabineau@homesite.com>
2019-06-03 01:09:18 -0400

    added controls for the parts of the burger

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import classes from './BuildControls.css';
-
 import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'},
+];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        <BuildControl label="test"/>
+        {controls.map((control) => (
+            <BuildControl key={control.label} label={control.label} type={control.type} />
+        ))}
     </div>
 );
 
